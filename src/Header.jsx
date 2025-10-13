5833029c20413c582af3f4d3cc5faae8a8136826
Anastasia462208 <201141413+Anastasia462208@users.noreply.github.com>
2025-10-13 05:18:14 -0400

    Version 18: Save current changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,9 +3,9 @@ import siriusLogo from './assets/images/sirius_logo.jpg'
 
 function Header() {
   return (
-    <div className="w-full bg-gradient-to-r from-blue-700 to-blue-900 text-white p-2 flex items-center justify-center shadow-md">
-      <img src={siriusLogo} alt="Логотип Сириус" className="h-8 mr-3" />
-      <h2 className="text-lg font-semibold">Многопрофильная гимназия Сириус</h2>
+    <div className="w-full bg-[#4A398F] text-white p-2 flex items-center justify-center shadow-md">
+      <img src={siriusLogo} alt="Логотип Сириус" className="h-12 mr-3" />
+      <h2 className="text-xl font-semibold">Многопрофильная гимназия Сириус</h2>
     </div>
   )
 }
